55fc780fccc8df39fb1ed1bc219ede68fa1463b3
Xavier Dumesnil <xavier@tech-angels.com>
2014-07-15 13:55:57 +0200

    Bump version

--- commands/app.go
+++ commands/app.go
@@ -10,7 +10,7 @@ func App() (*cli.App, error) {
 	app := cli.NewApp()
 	app.Name = "gemnasium"
 	app.Usage = "Gemnasium toolbelt"
-	app.Version = "0.2.3"
+	app.Version = "0.2.4"
 	app.Author = "Gemnasium"
 	app.Email = "support@gemnasium.com"
 	app.Flags = []cli.Flag{
